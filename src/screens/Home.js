f81bdb874ec712743964fdf59e31f04ea4cbd0e1
PedroDuvanel <pedrodbarboza@outlook.com>
2024-10-25 19:25:58 -0300

    Melhorando e corrijindo

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView, StatusBar } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
     const navigation = useNavigation();
     return (
         <View style={styles.container}>
+            <StatusBar barStyle="light-content" backgroundColor="#820ace" />
             <View style={styles.header}>
                 <View style={styles.profileContainer}>
+                    <View style={styles.userCard}>
+                        <Image source={require('../../assets/user.png')} style={styles.iconUser} />
+                    </View>
                     <Text style={styles.profile}>Olá, Pedro!</Text>
                 </View>
             </View>
             <View style={styles.balanceContainer}>
                 <Text style={styles.balanceLabel}>Conta</Text>
-                <Text style={styles.balance}>R$ 1.500.000,00</Text>
+                <Text style={styles.balance}>R$ 1.500,00</Text>
             </View>
-            <ScrollView 
-                horizontal 
-                showsHorizontalScrollIndicator={false} 
+            <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator={false}
                 contentContainerStyle={styles.actions}
             >
-                 <View style={styles.actionWrapper}>
+                <View style={styles.actionWrapper}>
                     <TouchableOpacity style={styles.actionButton} onPress={() => navigation.navigate('FazerPix')}>
                         <Image source={require('../../assets/pix.png')} style={styles.icon} />
                     </TouchableOpacity>
@@ -71,9 +75,10 @@ const Home = () => {
             </TouchableOpacity>
 
             <View style={styles.cardContainer}>
+                <View style={styles.separator} />
                 <Text style={styles.titleCard}>Cartão de crédito</Text>
                 <View style={styles.fattitles}>
-                    <Text style={styles.fatCard}>Fatura Atual</Text>
+                    <Text style={styles.fatCard}>Fatura atual</Text>
                     <Text style={styles.balanceCard}>R$ 1.489,00</Text>
                     <Text style={styles.limit}>Limite disponível de R$ 8.600,00</Text>
                 </View>
@@ -91,17 +96,33 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        height: 100,
-        backgroundColor: '#8A05BE',
+        height: 150,
+        backgroundColor: '#820ace',
         width: '100%',
     },
-    profileContainer: {
-        marginTop: 30,
+    pprofileContainer: {
+        marginTop: 20,
+    },
+    userCard: {
+        backgroundColor: '#9b3bda',
+        marginLeft: 20,
+        marginBottom: 15,
+        borderRadius: 30,
+        height: 50,
+        width: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     profile: {
         color: '#FFFFFF',
         fontSize: 20,
-        marginLeft: 40,
+        marginTop: 20,
+        marginLeft: 20,
+        marginBottom: 10,
+    },
+    iconUser: {
+        height: 25,
+        width: 25,
     },
     balanceContainer: {
         marginTop: 40,
@@ -116,33 +137,33 @@ const styles = StyleSheet.create({
     },
     balance: {
         color: '#000000',
-        fontSize: 32,
+        fontSize: 25,
         fontWeight: 'bold',
-        paddingRight: 120,
+        paddingRight: 235,
+        paddingBottom: 2,
     },
     actions: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,  // Ajustado para menos espaço
-        paddingBottom: 0, // Removido padding
+        marginTop: 20,
+        paddingBottom: 0,
     },
     actionWrapper: {
         alignItems: 'center',
-        marginRight: 10, 
+        marginRight: 10,
     },
     actionButton: {
-        width: 100,
-        height: 100,
+        width: 80,
+        height: 80,
         borderRadius: 50,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'lightgray',
+        backgroundColor: '#f0f1f5',
         marginLeft: 10,
     },
     icon: {
-        width: 30,
-        height: 30,
-        color: 'black',
+        width: 25,
+        height: 25,
     },
     actionText: {
         color: 'black',
@@ -151,13 +172,14 @@ const styles = StyleSheet.create({
         marginLeft: 12,
     },
     myCards: {
-        backgroundColor: '#D3D3D3',
+        backgroundColor: '#f0f1f5',
         width: '90%',
         alignSelf: 'center',
         borderRadius: 8,
         height: 60,
         paddingLeft: 30,
-        justifyContent: 'left', 
+        marginBottom: 10,
+        justifyContent: 'left',
         flexDirection: 'row',
     },
     myCardsView: {
@@ -177,17 +199,22 @@ const styles = StyleSheet.create({
     cardContainer: {
         flex: 1,
         padding: 20,
-        marginBottom: 200,
+        marginBottom: 220,
+    },
+    separator: {
+        height: 1, 
+        backgroundColor: '#f0f1f5', 
+        marginBottom: 20, 
+        width: '100%', 
     },
     fattitles: {
+        marginTop: 20,
         gap: 10,
     },
     titleCard: {
         color: '#000000',
         fontSize: 23,
         fontWeight: 'bold',
-        marginTop: 20,
-        marginBottom: 20,
     },
     fatCard: {
         color: 'grey',
